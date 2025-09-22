9d44122873298773f831b5b11b16fd1b6839da35
DharmX1 <101920887+DharmX1@users.noreply.github.com>
2025-09-22 16:17:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -772,7 +772,8 @@ async def txt_handler(bot: Client, m: Message):
                 keys_string = " ".join([f"--key {key}" for key in keys])
 
             elif "classplusapp.com/drm/" in url:
-                url = f"https://scammer-keys.vercel.app/api?url={url}&token={cptoken}&auth=@scammer_botxz1"
+                #url = f"https://scammer-keys.vercel.app/api?url={url}&token={cptoken}&auth=@scammer_botxz1"
+                url = f"https://covercel.vercel.app/extract_keys?url={url}@bots_updatee&user_id=1077356338"
                 mpd, keys = helper.get_mps_and_keys(url)
                 url = mpd
                 keys_string = " ".join([f"--key {key}" for key in keys])
